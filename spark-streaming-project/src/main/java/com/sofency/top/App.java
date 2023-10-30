9bc931ca635d72ad7f961662a17d1e9e0d852ea9
sofencyXiao <sofency@qq.com>
2023-10-30 09:51:47 +0800

    init

---
+++ spark-streaming-project/src/main/java/com/sofency/top/App.java
@@ -0,0 +1,13 @@
+package com.sofency.top;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
